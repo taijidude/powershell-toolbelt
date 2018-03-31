0048bf3eeff82d966442c9bacb64aec85f0796f7
patrick <p.r2@gmx.de>
2018-03-31 11:00:56 +0200

    test insert hinbekommen

--- Modules/timemaster/test.ps1
+++ Modules/timemaster/test.ps1
@@ -3,9 +3,22 @@
 Add-Type -Path "C:\Users\patrick\Documents\WindowsPowerShell\Scripts\powershell-toolbelt\Modules\timemaster\data-provider\System.Data.SQLite.dll"
 $con = New-Object -TypeName System.Data.SQLite.SQLiteConnection
 $con.ConnectionString = "Data Source=C:\Users\patrick\Documents\WindowsPowerShell\Scripts\powershell-toolbelt\Modules\timemaster\tracking-data.db"
+$con.Open()
+
 $sql = $con.CreateCommand()
 $sql.CommandText = "Select * from testtab"
 $adapter = New-Object -TypeName System.Data.SQLite.SQLiteDataAdapter $sql
 $data = New-Object System.Data.DataSet
 [void]$adapter.Fill($data)
 $data.Tables.rows
+$sql.Dispose()
+$con.Close()
+
+$con = New-Object -TypeName System.Data.SQLite.SQLiteConnection
+$con.ConnectionString = "Data Source=C:\Users\patrick\Documents\WindowsPowerShell\Scripts\powershell-toolbelt\Modules\timemaster\tracking-data.db"
+$con.Open()
+$sql = $con.CreateCommand()
+$sql.CommandText = "Insert into testtab values ('Das hier ist über den Data Provider');"
+$sql.ExecuteNonQuery()
+$sql.Dispose()
+$con.Close()
